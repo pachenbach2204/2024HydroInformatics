c0093ce5dcbba5d7efb600d2e5e6b58c4f35b7f4
pachenbach2204 <158511519+pachenbach2204@users.noreply.github.com>
2024-02-01 12:47:33 -0700

    Delete temperature_converter.py
    Deleted Temperature example code

--- SRC/code/temperature_converter.py
+++
@@ -1,8 +0,0 @@
-# ----------------------------------------------
-# Convert a temperature in Fahrenheit to Celsius
-# ----------------------------------------------
-Fahrenheit = 85.2
-
-Celsius = (Fahrenheit - 32) * 5.0 / 9.0
-
-print("Temperature:", Fahrenheit, "Fahrenheit = ", Celsius, " Celsius")
